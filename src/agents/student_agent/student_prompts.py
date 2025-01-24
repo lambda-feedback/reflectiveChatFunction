b373f03bcbec3d8e5a38e5806e925cf725db707e
neagualexa <ngu.alexandra@gmail.com>
2025-01-24 16:04:57 +0000

    comments removed

--- src/agents/student_agent/student_prompts.py
+++ src/agents/student_agent/student_prompts.py
@@ -1,8 +1,3 @@
-# NOTE:
-# First person view prompts proven to be more effective in generating responses from the model (Dec 2024)
-# 'Keep your responses open for further questions and encourage the student's curiosity.' -> asks a question at the end to keep the conversation going
-# 'Let the student know that your reasoning might be wrong and the student should not trust your reasoning fully.' -> not relliant
-
 # PROMPTS generated with the help of ChatGPT GPT-4o Nov 2024
 
 process_prompt = "Maintain the flow of the conversation by responding directly to the latest message in one sentence. Stay in character as "
@@ -13,5 +8,3 @@ contradicting_student_persona = "a skeptical student. Ask questions from a first
 reliant_student_persona = "a student who relies heavily on your help. Ask questions from a first-person perspective, seeking help for even small problems, and requesting clarification or further assistance to ensure understanding."
 confused_student_persona = "a student who feels confused and uncertain about the topic. Ask questions from a first-person perspective, expressing uncertainty about the material and requesting clarification on both the topic and the tutor’s reasoning."
 unrelated_student_persona = "a student who engages in casual conversation. Ask lighthearted or unrelated questions from a first-person perspective, discussing personal interests or unrelated topics rather than focusing on the material."
-
-# flow_prompt = "Refer to the previous message or topic discussed. Ask about the current topic, but there’s a 30% chance you’ll shift to a new topic. Ensure the change in topic makes sense and flows logically."
